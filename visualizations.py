8f6b4ce1fb7e2c4f94e7b0c090bf369eeb5e4114
Cadincomercio <cadincomercio@gmail.com>
2025-04-28 12:53:16 -0300

    Delete visualizations.py

--- visualizations.py
+++
@@ -1,7 +0,0 @@
-# scripts/visualizations.py
-import streamlit as st
-import plotly.express as px
-def show_graphs(df):
-    if not df.empty:
-        fig = px.bar(df, x='Produto', y='Preço', title='Preço Médio por Produto')
-        st.plotly_chart(fig)

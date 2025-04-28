5c0ffe1f0681210968b8da682e6ddac85e329237
Cadincomercio <cadincomercio@gmail.com>
2025-04-28 12:52:33 -0300

    Delete mercadolivre_scraper.py

--- mercadolivre_scraper.py
+++
@@ -1,4 +0,0 @@
-# scripts/mercadolivre_scraper.py
-import pandas as pd
-def scrape_ml(cidades, categoria, palavra_chave):
-    return pd.DataFrame({'Produto': [], 'Cidade': [], 'Vendas': [], 'Preço': [], 'Origem': []})
